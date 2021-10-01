9ac8482c6b08199fe0c51a165de1ab56dcf9cb6b
jaykomarraju <87209130+jaykomarraju@users.noreply.github.com>
2021-10-01 02:50:38 -0400

    Sept30

---
+++ ontecha/src/Components/SideNav/SideNav.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class SideBar extends Component{
+    render(){
+        return(
+            <ul>
+                <li>Hi</li>
+                <li>Hi</li>
+                <li>Hi</li>
+                <li>Hi</li>
+                <li>Hi</li>
+            </ul>
+        )
+    } 
+}
