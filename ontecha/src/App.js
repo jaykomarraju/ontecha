8643cea0320d59711847198dc8b05b33c3603c09
jaykomarraju <87209130+jaykomarraju@users.noreply.github.com>
2021-10-30 04:06:37 -0400

    Revert "OCT 26 -1"
    This reverts commit 52dfc2aee2f78b3eaa93a39938a1dc1872fad02d.

--- ontecha/src/App.js
+++ ontecha/src/App.js
@@ -5,7 +5,6 @@ import Plan from './pages/Plan';
 import Calendar from './pages/Calendar'
 import IdeaList from './pages/IdeaList'
 import MyContent from './pages/MyContent'
-import DayPage from './pages/DayPage';
 
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Route path='/' exact component={Calendar}/>
         <Route path='/idealist' component={IdeaList}/>
         <Route path='/mycontent' component={MyContent}/>
-        <Route path='/dayPage' component={DayPage}/>
       </Switch>
     </Router>
     </>
